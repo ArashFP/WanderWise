62dd0f65ece7d6e6d3a605421144287172bba9a9
Arash Falah Parsa <arash_afp_94@hotmail.com>
2024-11-04 03:55:56 +0100

    adds profilePage

---
+++ app/profile/page.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../_components/navbar_mobile"
+
+const ProfilePage = () => {
+  return (
+    <main>
+      ProfilePage
+
+      <Navbar />
+    </main>
+  )
+}
+export default ProfilePage
