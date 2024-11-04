c34063425f7804efe297dac7aa70558c4bd7cd88
Arash Falah Parsa <arash_afp_94@hotmail.com>
2024-11-04 05:56:33 +0100

    adds category, priceslider, calendar

--- app/search_page/page.jsx
+++ app/search_page/page.jsx
@@ -2,9 +2,12 @@
 import { CircleArrowLeft } from "lucide-react"
 import { GuestDropDownMenu } from "./_components/guestDropDownMenu"
 import { Navbar } from "../_components/navbar_mobile"
+import { CategoryDropDown } from "./_components/categoryDropDown"
+import { PriceSliderDropdown } from "./_components/priceSliderDropdown"
+import { Calendar } from "./_components/calendar"
 
 const searchpage = () => {
-  
+
   return (
     <main className="bg-timberwolf h-screen pt-7">
       <div className="flex items-center justify-between">
@@ -18,6 +21,12 @@ const searchpage = () => {
           className="w-72 h-10 p-4 bg-fernGreen text-timberwolf placeholder:text-timberwolf rounded-xl"
         />
         <GuestDropDownMenu />
+        <CategoryDropDown />
+        <PriceSliderDropdown />
+        <Calendar />
+        <button className="w-72 absolute bottom-28 mt-2 p-2 bg-BrunswickGreen text-timberwolf rounded-xl">
+          Search
+        </button>
       </div>
       <Navbar />
     </main>
