85fc953f6fcb57e284d07fd514487937ed9ce6b8
Arash Falah Parsa <arash_afp_94@hotmail.com>
2024-11-04 03:52:40 +0100

    finished landing page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,21 @@
+import { Navbar } from './_components/navbar_mobile'
+import { ListingCard } from './_components/listingCard'
+import { Searchbar_mobile } from './_components/searchbar_mobile'
 
 const LandingPage = () => {
-  return (
-    <main className="bg-timberwolf h-screen">
 
+
+
+  
+  return (
+    <main className="bg-timberwolf h-screen pt-3">
+      <div className='flex items-center justify-between w-full px-4'>
+        <img src="/logo.png" alt="hej" className='w-22 h-10' />
+        <Searchbar_mobile/>
+      </div>
+      <ListingCard/>
+      <Navbar/>
     </main>
   )
 }
-export default LandingPage
+export default LandingPage 
