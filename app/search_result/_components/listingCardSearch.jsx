54e131b3fe4c8ba994b2c63338d099b47c5eb58d
Arash Falah Parsa <arash_afp_94@hotmail.com>
2024-11-04 08:14:11 +0100

    adds component, currently filtering only by category

---
+++ app/search_result/_components/listingCardSearch.jsx
@@ -0,0 +1,72 @@
+'use client'
+import axios from "axios"
+import { Dot, Star } from "lucide-react"
+import { useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react"
+
+export const ListingCard = () => {
+  const searchParams = useSearchParams()
+  const [listings, setListings] = useState([])
+  const [filteredListings, setFilteredListings] = useState([])
+
+  const fetchListings = async () => {
+    try {
+      const response = await axios.get("/api/listings")
+      const listings = response.data
+      setListings(listings)
+      console.log('ALL Listings fetched')
+    } catch (error) {
+      console.log("error", error)
+    }
+  }
+
+  const filterListings = () => {
+    const categories = searchParams.get('categories')?.split(',') || []
+
+    const filteredListings = listings.filter(listing => {
+      const matchesCategories = categories.length > 0 ? categories.some(category => listing.categories.includes(category)) : true
+
+      return matchesCategories
+    })
+
+    setFilteredListings(filteredListings)
+    console.log('Filtered Listings:', filteredListings)
+  }
+
+  useEffect(() => {
+    fetchListings()
+  }, [])
+
+  useEffect(() => {
+    if (listings.length > 0) {
+      filterListings()
+    }
+  }, [searchParams, listings])
+
+  return (
+    <div>
+      {filteredListings.map((listing) => (
+        <div key={listing.id} className="bg-timberwolf w-80 h-52 rounded-l p-4 m-4 gap-3">
+          <img
+            src={listing.images[0].url}
+            alt="Listing Image"
+            className="w-full h-full object-cover mt-2 rounded-xl"
+          />
+          <div className="flex">
+            <div>
+              <p>{listing.price}Euro/Night</p>
+              <p>{listing.title}</p>
+            </div>
+            <div className="bg-timberwolf flex items-center justify-center ml-auto">
+              <Star className="text-iconColor fill-iconColor" />
+              <p className="font-bold">
+                <span> {listing.rating} </span>
+                <span className="underline"> {listing.reviews} reviews </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
