72be9ce55962da1cc9157102d1df4dbc9886146a
Arash Falah Parsa <arash_afp_94@hotmail.com>
2024-11-10 11:47:15 +0100

    adds styling to detailpage

--- app/detail_page/[id]/page.jsx
+++ app/detail_page/[id]/page.jsx
@@ -1,35 +1,35 @@
-'use client'
-import { Navbar } from "@/app/_components/navbar_mobile"
-import ProfileBar from "@/app/_components/profileBar"
-import { ListingCardSingle } from "./_components/listingCardSingle"
-import { useSearchParams } from "next/navigation"
-import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
+"use client";
+import { Navbar } from "@/app/_components/navbar_mobile";
+import ProfileBar from "@/app/_components/profileBar";
+import { ListingCardSingle } from "./_components/listingCardSingle";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const detailPage = () => {
-  const searchParams = useSearchParams();
   const router = useRouter();
-  const [id, setId] = useState('');
+  const [id, setId] = useState("");
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       const fullPath = window.location.pathname + window.location.search;
-      const relativePath = fullPath.replace('/detail_page', '');
+      const relativePath = fullPath.replace("/detail_page", "");
       setId(relativePath); // Get everything after /detail_page
-      console.log(relativePath + " " + "id from [id] page.jsx")
+      console.log(relativePath + " " + "id from [id] page.jsx");
     }
   }, [router]);
 
-
   return (
-    <main className="bg-timberwolf h-screen pt-3">
-      <div className='flex items-center justify-between w-full px-4'>
-        <img src="/logo.png" alt="hej" className='w-22 h-10' />
+    <main className="bg-timberwolf flex flex-col h-screen pt-3">
+      <div className="flex items-center justify-between w-full px-4">
+        <img src="/logo.png" alt="hej" className="w-22 h-10" />
         <ProfileBar />
       </div>
-      <ListingCardSingle id={id}/>
-      <Navbar />
+      <div className="flex-grow overflow-auto mb-24">
+        <ListingCardSingle id={id} />
+      </div>
+
+      <Navbar className="" />
     </main>
-  )
-}
-export default detailPage
+  );
+};
+export default detailPage;
