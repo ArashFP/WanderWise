71ea2524281ad512e0a66d0882ce729a744ce1bb
Arash Falah Parsa <arash_afp_94@hotmail.com>
2024-11-14 20:27:24 +0100

    adds searchbar to detailpage

--- app/detail_page/[id]/page.jsx
+++ app/detail_page/[id]/page.jsx
@@ -5,6 +5,7 @@ import { ListingCardSingle } from "./_components/listingCardSingle";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { Footer } from "@/app/_components/footer";
+import { Searchbar } from "@/app/_components/searchbar";
 
 const detailPage = () => {
   const router = useRouter();
@@ -22,14 +23,15 @@ const detailPage = () => {
     <main className="bg-timberwolf flex flex-col h-screen pt-3">
       <div className="flex items-center justify-between w-full px-4">
         <img src="/logo.png" alt="hej" className="w-22 h-10" />
+        <Searchbar />
         <ProfileBar />
       </div>
       <div className="flex-grow overflow-auto mb-24">
         <ListingCardSingle id={id} />
       </div>
 
-      <Navbar/>
-      <Footer/>
+      <Navbar />
+      <Footer />
     </main>
   );
 };
