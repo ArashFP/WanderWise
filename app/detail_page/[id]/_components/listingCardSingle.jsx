536c8cdca254f695a0077f637e5047cec7f1a971
Arash Falah Parsa <arash_afp_94@hotmail.com>
2024-11-14 12:40:12 +0100

    adds icons

--- app/detail_page/[id]/_components/listingCardSingle.jsx
+++ app/detail_page/[id]/_components/listingCardSingle.jsx
@@ -10,12 +10,20 @@ import {
   BedSingle,
   User,
   ChevronDown,
+  Dumbbell,
+  Trees,
+  Backpack,
+  FlameKindling,
+  WifiOff,
+  Caravan,
 } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import getDocument from "../../../../lib/getDocument";
 import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
 import { format, differenceInDays } from "date-fns";
+import { useRouter } from "next/navigation";
+import { BookingContext } from "@/lib/context/bookingContext";
 
 export const ListingCardSingle = ({ id }) => {
   const [listing, setListing] = useState(null);
@@ -23,24 +31,31 @@ export const ListingCardSingle = ({ id }) => {
   const [adults, setAdults] = useState(1);
   const [children, setChildren] = useState(0);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [checkInDate, setCheckInDate] = useState(null);
-  const [checkOutDate, setCheckOutDate] = useState(null);
   const [isCheckInOpen, setIsCheckInOpen] = useState(false);
   const [isCheckOutOpen, setIsCheckOutOpen] = useState(false);
+
+  const { setTotalPrice, setCheckInDate, setCheckOutDate } = useContext(BookingContext);
+  const [checkInDate, setLocalCheckInDate] = useState(null);
+  const [checkOutDate, setLocalCheckOutDate] = useState(null);
+  
+  const router = useRouter();
 
   const amenitiesIcons = {
     House: <House className="text-iconColor" />,
     AC: <AirVent className="text-iconColor" />,
     Shower: <ShowerHead className="text-iconColor" />,
     Wifi: <Wifi className="text-iconColor" />,
-    NoWifi: <Wifi className="text-iconColor" />,
+    NoWifi: <WifiOff className="text-iconColor" />,
     DoubleBed: <BedDouble className="text-iconColor" />,
     SingleBed: <BedSingle className="text-iconColor" />,
+    OutdoorFirePit: <FlameKindling className="text-iconColor" />,
+    CampingFriendly: <Caravan className="text-iconColor" />,
+    StorageRoom: <Backpack className="text-iconColor" />,
+    Nautre: <Trees className="text-iconColor" />,
+    Gym: <Dumbbell className="text-iconColor" />,
   };
 
   useEffect(() => {
-    console.log("Document IDd in listingCardSingle", id);
-
     if (!id) return;
 
     const fetchListing = async () => {
@@ -54,6 +69,16 @@ export const ListingCardSingle = ({ id }) => {
 
     fetchListing();
   }, [id]);
+
+  useEffect(() => {
+    if (!checkInDate || !checkOutDate || !listing) return;
+  
+    const numberOfDays = differenceInDays(checkOutDate, checkInDate);
+    const totalPrice = listing.price * numberOfDays;
+    setTotalPrice(totalPrice);
+    setCheckInDate(checkInDate);
+    setCheckOutDate(checkOutDate);
+  }, [checkInDate, checkOutDate, listing, setTotalPrice, setCheckInDate, setCheckOutDate]);
 
   const incrementAdults = () => setAdults(adults + 1);
   const decrementAdults = () => setAdults(adults > 1 ? adults - 1 : 1);
@@ -80,6 +105,10 @@ export const ListingCardSingle = ({ id }) => {
       : 0;
   const totalPrice = listing.price * numberOfDays;
 
+  const handleBookNowClick = () => {
+    router.push("/pay" + id);
+  };
+  
   return (
     <div>
       <div className="bg-timberwolf w-80 h-48 rounded-l p-4 m-4 mt-7 flex flex-col">
@@ -90,32 +119,35 @@ export const ListingCardSingle = ({ id }) => {
         />
         <div className="flex">
           <div>
-            <p>{listing.price} Euro/Night</p>
-            <p>{listing.title}</p>
+            <p className="text-black">{listing.price} Euro/Night</p>
+            <p className="text-black">{listing.title}</p>
           </div>
           <div className="bg-timberwolf flex items-center justify-center ml-auto">
             <Star className="text-iconColor fill-iconColor" />
             <p className="font-bold">
-              <span> {listing.rating} </span>
-              <span className="underline"> {listing.reviews} reviews </span>
+              <span className="text-black"> {listing.rating} </span>
+              <span className="underline text-black">
+                {" "}
+                {listing.reviews} reviews{" "}
+              </span>
             </p>
           </div>
         </div>
         <div>
-          <p className="text-sm mt-2">{listing.description}</p>
+          <p className="text-sm mt-2 text-black">{listing.description}</p>
         </div>
         <hr className="bg-BrunswickGreen mt-2 mb-3 opacity-100" />
         <div className="flex flex-wrap gap-3 justify-between w-full">
           {listing.amenities.map((amenity, index) => (
             <div key={index} className="flex flex-col items-center mr-4">
               {amenitiesIcons[amenity] || amenity}
-              <span className="text-sm">{amenity}</span>
+              <span className="text-sm text-black">{amenity}</span>
             </div>
           ))}
         </div>
         <div className="bg-timberwolf border-darkGreen border-2 rounded-xl mt-3 p-1 px-2">
-          <p> Cancellation Policy:</p>
-          <p className="text-sm">{listing.cancellation_policy}</p>
+          <p className="text-black"> Cancellation Policy:</p>
+          <p className="text-sm text-black">{listing.cancellation_policy}</p>
         </div>
         <div className="bg-timberwolf border-darkGreen border-2 rounded-xl mt-3 p-1 px-2 flex items-center">
           {owner.image ? (
@@ -128,14 +160,14 @@ export const ListingCardSingle = ({ id }) => {
             <User className="w-10 h-10 text-iconColor fill-iconColor mr-2" />
           )}
           <div>
-            <p className="text-sm font-bold">{owner.name}</p>
+            <p className="text-sm text-black font-bold">{owner.name}</p>
             <p className="text-sm">
-              <span className="font-bold">Phone:</span>{" "}
-              <span className="underline">{owner.phone}</span>
+              <span className="font-bold text-black">Phone:</span>{" "}
+              <span className="underline text-black">{owner.phone}</span>
             </p>
             <p className="text-sm">
-              <span className="font-bold">Email:</span>{" "}
-              <span className="underline">{owner.email}</span>
+              <span className="font-bold text-black">Email:</span>{" "}
+              <span className="underline text-black">{owner.email}</span>
             </p>
           </div>
         </div>
@@ -144,11 +176,11 @@ export const ListingCardSingle = ({ id }) => {
         </div>
         <div className="bg-timberwolf border-darkGreen border-2 rounded-xl mt-3 p-1 px-2">
           <div className="flex flex-col gap-3 items-center justify-between">
-            <p className="text-xl">
+            <p className="text-xl text-black">
               {"Price per night " + listing.price + " Euro"}
             </p>
             <p
-              className="text-lg font-bold cursor-pointer border-BrunswickGreen border-2 w-2/3 flex justify-between px-2"
+              className="text-lg text-black font-bold cursor-pointer border-BrunswickGreen border-2 w-2/3 flex justify-between px-2"
               onClick={toggleDropdown}
             >
               Guests <ChevronDown />
@@ -156,36 +188,36 @@ export const ListingCardSingle = ({ id }) => {
             {isDropdownOpen && (
               <div className="flex flex-col justify-between items-center mt-2">
                 <div className="flex justify-between items-center w-2/3">
-                  <p className="mr-2">Adults</p>
+                  <p className="mr-2 text-black">Adults</p>
                   <div className="flex items-center">
                     <button
                       onClick={decrementAdults}
-                      className="px-2 py-1 bg-gray-300 rounded"
+                      className="px-2 py-1 text-black bg-gray-300 rounded"
                     >
                       -
                     </button>
-                    <p className="mx-2">{adults}</p>
+                    <p className="mx-2 text-black">{adults}</p>
                     <button
                       onClick={incrementAdults}
-                      className="px-2 py-1 bg-gray-300 rounded"
+                      className="px-2 py-1 text-black bg-gray-300 rounded"
                     >
                       +
                     </button>
                   </div>
                 </div>
                 <div className="flex justify-between items-center w-2/3 mt-2">
-                  <p className="mr-2">Children</p>
+                  <p className="mr-2 text-black">Children</p>
                   <div className="flex items-center">
                     <button
                       onClick={decrementChildren}
-                      className="px-2 py-1 bg-gray-300 rounded"
+                      className="px-2 py-1 text-black bg-gray-300 rounded"
                     >
                       -
                     </button>
-                    <p className="mx-2">{children}</p>
+                    <p className="mx-2 text-black">{children}</p>
                     <button
                       onClick={incrementChildren}
-                      className="px-2 py-1 bg-gray-300 rounded"
+                      className="px-2 py-1 text-black bg-gray-300 rounded"
                     >
                       +
                     </button>
@@ -199,7 +231,7 @@ export const ListingCardSingle = ({ id }) => {
               className="flex flex-col items-center cursor-pointer border-BrunswickGreen border-2 w-1/2 relative"
               onClick={toggleCheckIn}
             >
-              <p className="mr-2 flex justify-center items-center">
+              <p className="mr-2 text-black flex justify-center items-center">
                 {checkInDate
                   ? format(checkInDate, "dd/MM/yyyy")
                   : "Check-in Date"}{" "}
@@ -209,7 +241,7 @@ export const ListingCardSingle = ({ id }) => {
                 <div className="absolute top-full left-0 w-full z-10">
                   <DatePicker
                     selected={checkInDate}
-                    onChange={(date) => setCheckInDate(date)}
+                    onChange={(date) => setLocalCheckInDate(date)}
                     inline
                   />
                 </div>
@@ -219,7 +251,7 @@ export const ListingCardSingle = ({ id }) => {
               className="flex flex-col items-center cursor-pointer border-BrunswickGreen border-2 w-1/2 relative"
               onClick={toggleCheckOut}
             >
-              <p className="mr-2 flex justify-center items-center">
+              <p className="mr-2 text-black flex justify-center items-center">
                 {checkOutDate
                   ? format(checkOutDate, "dd/MM/yyyy")
                   : "Check-in Date"}{" "}
@@ -229,7 +261,7 @@ export const ListingCardSingle = ({ id }) => {
                 <div className="absolute top-full right-full w-full z-10">
                   <DatePicker
                     selected={checkOutDate}
-                    onChange={(date) => setCheckOutDate(date)}
+                    onChange={(date) => setLocalCheckOutDate(date)}
                     inline
                   />
                 </div>
@@ -238,29 +270,32 @@ export const ListingCardSingle = ({ id }) => {
           </div>
           <hr className="bg-BrunswickGreen mt-4 mb-4 opacity-100" />
           <div className="flex justify-between">
-            <p className="text-lg">
+            <p className="text-lg text-black">
               {listing.price} x {numberOfDays} Days
             </p>
-            <p className="text-lg">{totalPrice} Euro</p>
+            <p className="text-lg text-black">{totalPrice} Euro</p>
           </div>
           <div className="flex justify-between">
-            <p className="text-lg">Cleaning Fee</p>
-            <p className="text-lg">{listing.cleaning_fee} Euro</p>
+            <p className="text-lg text-black">Cleaning Fee</p>
+            <p className="text-lg text-black">{listing.cleaning_fee} Euro</p>
           </div>
           <div className="flex justify-between">
-            <p className="text-lg">WanderWise Fee</p>
-            <p clasName="text-lg">1 Euro</p>
+            <p className="text-lg text-black">WanderWise Fee</p>
+            <p className="text-lg text-black">1 Euro</p>
           </div>
           <hr className="bg-BrunswickGreen mt-4 mb-4 opacity-100" />
           <div className="flex justify-between mb-3">
-            <p className="text-lg">Total</p>
-            <p className="text-lg">
+            <p className="text-lg text-black">Total</p>
+            <p className="text-lg text-black">
               {totalPrice + listing.cleaning_fee + 1} Euro
             </p>
           </div>
         </div>
         <div className="flex justify-center w-full mt-4 mb-4">
-          <button className="bg-BrunswickGreen text-white w-full p-2 rounded-xl">
+          <button
+            className="bg-BrunswickGreen text-white w-full p-2 rounded-xl"
+            onClick={handleBookNowClick}
+          >
             Book Now
           </button>
         </div>
